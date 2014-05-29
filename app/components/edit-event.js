8e4b8111c22d7708dac845cb6ec319be1ee8fc20
Anton <anton.lantsov@gmail.com>
2014-05-29 10:57:58 +0400

    create and edit event

--- app/components/edit-event.js
+++ app/components/edit-event.js
@@ -1,5 +1,35 @@
 var EditEventComponent = Ember.Component.extend({
-
+  directions: ['', '>>', '<<', '><'],
+  event: function(){
+    var currentEvent = this.get("agent.events.firstObject");
+    if(currentEvent == null){
+      currentEvent = this.get('targetObject.store').createRecord('event',
+      {agent_id: this.get("agent.id")});
+    }
+    return currentEvent;
+  }.property("agent.events.firstObject"),
+  actions: {
+    saveEvent: function(){
+      if(this.get('event.direction')){
+        var persisted = this.get('event.id') != null;
+        this.get('event').save();
+        if(!persisted){
+          this.get('agent.events').addObject(this.get('event'));
+        }
+      }else{
+        var record = this.get('event');
+        record.deleteRecord();
+        record.save();
+      }
+      return false;
+    },
+    removeEvent: function(){
+      var record = this.get('event');
+      record.deleteRecord();
+      record.save();
+      return false;
+    }
+  }
 });
 
 export default EditEventComponent;
--- app/models/event.js
+++ app/models/event.js
@@ -1,7 +1,7 @@
 export default DS.Model.extend({
   direction: DS.attr('string'),
   action: DS.attr('string'),
-  date: DS.attr('date'),
+  date: DS.attr('date',{defaultValue: new Date()}),
   agent: DS.belongsTo('agent'),
   humanDate: function(){
     return this.get("date").toLocaleDateString();
